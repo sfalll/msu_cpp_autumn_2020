e4ca7bc455fe226d8fd3b39f1b59a71de55f138e
sfalll <alexkrasnih0914@gmail.com>
2020-10-14 22:56:14 +0300

    Update allocator.cpp

--- 01/allocator.cpp
+++ 01/allocator.cpp
@@ -3,25 +3,10 @@
 
 
 void Allocator::MakeAllocator(size_t maxSize){
-    if(start_ == nullptr){
-        maxSize_ = maxSize;
-        start_ = new char[maxSize];
-        if(start_ == nullptr){
-            std::cout << "Can't alloc memory" << std::endl;
-            exit(-1);
-        }
-    }
-    else{
-        delete[] start_;
-        start_ = nullptr;
-        maxSize_ = maxSize;
-        offset_ = 0;
-        start_ = new char[maxSize];
-        if(start_ == nullptr){
-            std::cout << "Can't alloc memory" << std::endl;
-            exit(-1);
-        }
-    }
+    delete[] start_;
+    maxSize_ = maxSize;
+    offset_ = 0;
+    start_ = new char[maxSize];
 }
 
 char* Allocator::Alloc(size_t size){
